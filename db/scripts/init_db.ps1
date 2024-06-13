ceab6ab35f60c7f7fd1c6c8ff56ce5d00c8930a0
Guanart <gu4n4rt@gmail.com>
2024-06-13 17:38:37 -0300

    init_db.ps1

---
+++ db/scripts/init_db.ps1
@@ -0,0 +1,9 @@
+# Crear la base de datos
+.\vendor\robmorgan\phinx\bin\phinx rollback -t 0
+.\vendor\robmorgan\phinx\bin\phinx migrate
+.\vendor\robmorgan\phinx\bin\phinx seed:run -s UserSeeder
+.\vendor\robmorgan\phinx\bin\phinx seed:run -s VideoCardSeeder
+.\vendor\robmorgan\phinx\bin\phinx seed:run -s MotherboardSeeder
+.\vendor\robmorgan\phinx\bin\phinx seed:run -s MemorySeeder
+.\vendor\robmorgan\phinx\bin\phinx seed:run -s InternalHardDriveSeeder
+.\vendor\robmorgan\phinx\bin\phinx seed:run -s CpuFanSeeder
